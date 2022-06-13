a585994292798f6299b8f585fc3daa592ebf8d5c
your Name <mohamedabdulayubm@gmail.com>
2022-06-13 19:35:35 +0530

    Add project files.

---
+++ PartTimeWage/PartTimeWage/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+        int EMP_PRESENT = 1;
+        int EMP_PART = 2;
+        int EMP_SALARY = 50;
+
+        Random num = new Random();
+        int ran = num.Next(0, 3);
+
+        int empWrkHrs = 0;
+
+        if (ran == EMP_PRESENT)
+        {
+            Console.WriteLine("Is Present");
+            empWrkHrs = 16;
+
+
+        }
+        else if (ran == EMP_PART)
+        {
+            Console.WriteLine("Is Part-Time");
+            empWrkHrs = 8;
+
+
+        }
+        else
+        {
+            Console.WriteLine("Is Absent");
+            empWrkHrs = 0;
+
+
+        }
+
+        int empWage = (empWrkHrs * EMP_SALARY);
+        Console.WriteLine("The total salary is :" + empWage);
